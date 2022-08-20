6f2adeaf6d080bce54e75fda119176996fcd08cf
ersin göç <ersin_1229@hotmail.com>
2022-08-20 12:56:06 +0300

    Formik added

---
+++ 11-formik/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Genelformyapisi from './component/Genelformyapisi'
+import Signup from './component/Signup'
+function App() {
+
+ 
+
+  return (
+    <div className="App">
+      {/* <Genelformyapisi /> */}
+      <Signup />
+    </div >
+  );
+}
+
+export default App;
